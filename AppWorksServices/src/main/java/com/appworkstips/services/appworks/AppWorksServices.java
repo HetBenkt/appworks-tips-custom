bb6cec84d37988108c3d44843703ad2a59875c07
Antal Bos <HetBenkt@gmail.com>
2019-10-21 16:59:38 +0200

    Playing with some webservices, but String is the only acceptable return type

---
+++ AppWorksServices/src/main/java/com/appworkstips/services/appworks/AppWorksServices.java
@@ -0,0 +1,65 @@
+package com.appworkstips.services.appworks;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class AppWorksServices {
+    private final static Logger LOGGER = Logger.getLogger(AppWorksServices.class.getName());
+
+    public static String getRandomIntValue(String minValue, String maxValue) {
+        String result = "";
+        Random r = new Random();
+        int max = 0;
+        int min = 0;
+
+        try {
+            max = Integer.parseInt(maxValue);
+            min = Integer.parseInt(minValue);
+            result = String.valueOf(r.nextInt((max - min) + 1) + min);
+        } catch (NumberFormatException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            return "-1";
+        } catch (IllegalArgumentException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            return "-1";
+        }
+
+        LOGGER.info(String.format("Random int: %s", result));
+        return result;
+
+    }
+
+    public static String getRandomIntValue() {
+        Random r = new Random();
+        String result = String.valueOf(r.nextInt());
+        LOGGER.info(String.format("Random int: %s", result));
+        return result;
+
+    }
+
+    public static int getRandomInt() {
+        Random r = new Random();
+        int result = r.nextInt();
+        LOGGER.info(String.format("Random int: %s", result));
+        return result;
+    }
+
+    public static Integer getRandomIntObject() {
+        Random r = new Random();
+        int result = r.nextInt();
+        LOGGER.info(String.format("Random int: %s", result));
+        return result;
+    }
+
+    public static List<Integer> getRandomIntObjectList() {
+        Random r = new Random();
+        List<Integer> result = new ArrayList<Integer>();
+        result.add(r.nextInt());
+        LOGGER.info(String.format("Random int: %s", result));
+        return result;
+    }
+
+}
---
+++ AppWorksServices/src/test/java/com/appworkstips/services/appworks/AppWorksServicesTest.java
@@ -0,0 +1,51 @@
+package com.appworkstips.services.appworks;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class AppWorksServicesTest {
+
+    @Test
+    public void getRandomIntValue() {
+        String randomInt = AppWorksServices.getRandomIntValue();
+        Assert.assertNotEquals("-1", randomInt);
+    }
+
+    @Test
+    public void getRandomIntValueNormalInput() {
+        String randomInt = AppWorksServices.getRandomIntValue("0", "2");
+        Assert.assertNotEquals("-1", randomInt);
+    }
+
+    @Test
+    public void getRandomIntValueWrongInput() {
+        String randomInt = AppWorksServices.getRandomIntValue("a", "z");
+        Assert.assertEquals("-1", randomInt);
+    }
+
+    @Test
+    public void getRandomIntValueNegagiveInput() {
+        String randomInt = AppWorksServices.getRandomIntValue("-5", "-10");
+        Assert.assertEquals("-1", randomInt);
+    }
+
+    @Test
+    public void getRandomInt() {
+        int randomInt = AppWorksServices.getRandomInt();
+        Assert.assertTrue(randomInt >= Integer.MIN_VALUE && randomInt <= Integer.MAX_VALUE);
+    }
+
+    @Test
+    public void getRandomIntObject() {
+        Integer randomInteger = AppWorksServices.getRandomIntObject();
+        Assert.assertTrue(randomInteger >= Integer.MIN_VALUE && randomInteger <= Integer.MAX_VALUE);
+    }
+
+    @Test
+    public void getRandomIntObjectList() {
+        List<Integer> randomIntObjectList = AppWorksServices.getRandomIntObjectList();
+        Assert.assertTrue(randomIntObjectList.size() == 1);
+    }
+}
